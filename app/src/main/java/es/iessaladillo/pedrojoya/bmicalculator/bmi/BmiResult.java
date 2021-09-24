6550b0555e65132be8c58936e1bbea935448c249
pedrojoya <pedrojoya@gmail.com>
2021-09-24 12:18:51 +0200

    Initial commit

---
+++ app/src/main/java/es/iessaladillo/pedrojoya/bmicalculator/bmi/BmiResult.java
@@ -0,0 +1,11 @@
+package es.iessaladillo.pedrojoya.bmicalculator.bmi;
+
+public class BmiResult {
+
+    private static final float NORMAL_WEIGHT_LOWER_BOUND = 18.5f;
+    private static final float OVERWEIGHT_LOWER_BOUND = 25f;
+    private static final float OBESITY_CLASS_1_LOWER_BOUND = 30f;
+    private static final float OBESITY_CLASS_2_LOWER_BOUND = 35f;
+    private static final float OBESITY_CLASS_3_LOWER_BOUND = 40f;
+
+}
